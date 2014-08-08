e006b96abb192f55987004dfa3ea24723a142524
leandrodefreitas.1983@gmail.com <leandrodefreitas.1983@gmail.com@5e7338cb-f527-8b1e-bca7-e1c64bb0f196>
2014-08-08 03:07:41 +0000

    Ajustes das classes, inserção de comentário (parcial) e implementação de properties (completo) - Leandro

--- trunk/AtividadeFinal/src/br/com/fiap/sistema/conta/Movimento.java
+++ trunk/AtividadeFinal/src/br/com/fiap/sistema/conta/Movimento.java
@@ -7,15 +7,28 @@ public enum Movimento {
 	private String descricao;
 	private int codigo;
 
+	/**
+	 * 
+	 * @param descricao
+	 * @param codigo
+	 */
 	Movimento(String descricao, int codigo) {
 		this.descricao = descricao;
 		this.codigo = codigo;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public int getCodigo() {
 		return codigo;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getDescricao() {
 		return descricao;
 	}
--- trunk/AtividadeFinal/src/br/com/fiap/sistema/exceptions/ValorInvalidoException.java
+++ trunk/AtividadeFinal/src/br/com/fiap/sistema/exceptions/ValorInvalidoException.java
@@ -2,13 +2,14 @@ package br.com.fiap.sistema.exceptions;
 
 public class ValorInvalidoException extends RuntimeException{
 
+	/**
+	 * 
+	 * @param valor
+	 */
 	public ValorInvalidoException(double valor) {
 		super("Valor inválido: " + valor + ". Operação cancelada. \n");
 	}
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 }
