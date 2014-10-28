63d77538557c620e85e1ff87140d47cd0f738286
flaviomitsumi@gmail.com <flaviomitsumi@gmail.com@5e7338cb-f527-8b1e-bca7-e1c64bb0f196>
2014-10-28 04:24:23 +0000

    graficos

--- trunk/DesenvolvimentoAndroid/Financas/src/br/com/fiap/financas/activity/GraficosActivity.java
+++ trunk/DesenvolvimentoAndroid/Financas/src/br/com/fiap/financas/activity/GraficosActivity.java
@@ -1,20 +1,49 @@
 package br.com.fiap.financas.activity;
 
 import br.com.fiap.R;
-import br.com.fiap.financas.util.InteractiveLineGraphView;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.WebView;
 import android.app.Activity;
 
 public class GraficosActivity extends Activity {
-
-    private InteractiveLineGraphView mGraphView ;
+	
+	WebView wvGrafico;
+	String strURL;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.informacoes);
-		//mGraphView = (InteractiveLineGraphView) findViewById(R.id.chart);
+		setContentView(R.layout.graficos);
+		
+		strURL =
+		"http://chart.apis.google.com/chart?chxl=0:|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec&chxt=x,y&chs=300x300&cht=r&chco=FF0000&chd=t:63,64,67,73,77,81,85,86,85,81,74,67,63&chls=2,4,0&chm=B,FF000080,0,0,0";
+
+		/*strURL = "https://chart.googleapis.com/chart?" + "cht=lc&" + 
+					"chxt=x,y&" + // imprime os valores dos eixos X, Y
+					"chs=300x300&" + // define o tamanho da imagem
+					"chd=t:10,45,5,10,13,26&" + // valor de cada coluna do gráfico
+					"chl=Jan|Fev|Mar|Abr|Mai|Jun&" + // rótulo para cada coluna
+					"chdl=Vendas&" + // legenda do gráfico
+					"chxr=1,0,50&" + // define o valor de início e fim do eixo
+					"chds=0,50&" + // define o valor de escala dos dados
+					"chg=0,5,0,0&" + // desenha linha horizontal na grade
+					"chco=3D7930&" + // cor da linha do gráfico
+					"chtt=Vendas+x+1000&" + // cabeçalho do gráfico
+					"chm=B,C5D4B5BB,0,0,0" ; // fundo verde
+		*/
+		
+		// Abaixo seguem outros exemplo de gráfico:
+		//strURL =
+		//"https://chart.googleapis.com/chart?cht=lc&chxt=x,y&chs=300x300&chd=t:10,45,5,10,13,26&chl=Janeiro|Fevereiro|Marco|Abril|Maio|Junho&chdl=Vendas%20&chxr=1,0,50&chds=0,25&chg=0,5,0,0&chco=3D7930&chtt=Vendas+x+1000&chm=v,FF0000,0,::.10,4";
+		//strURL =
+		//"http://chart.apis.google.com/chart?cht=bhg&chs=550x400&chd=t:100,50,115,80|10,20,15,30&chxt=x,y&chxl=1:|Janeiro|Fevereiro|Marco|Abril&chxr=0,0,120&chds=0,120&chco=4D89F9&chbh=35,0,15&chg=8.33,0,5,0&chco=0A8C8A,EBB671&chdl=Vendas|Compras";
+		//strURL =
+		//"https://chart.googleapis.com/chart?cht=lc&chxt=x,y&chs=700x350&chd=t:10,45,5,10|30,35,30,15|10,10.5,30,35&chl=Janeiro|Fevereiro|Marco|Abril&chdl=Vendas|Compras|Outros&chxr=1,0,50&chds=0,50&chg=0,5,0,0&chco=DA3B15,3072F3,000000&chtt=grafico+de+vendas";
+		/*strURL =
+		"https://chart.googleapis.com/chart?cht=p3&chs=200x90&chd=t:40,45,5,10&chl=Jan|Fev|Mar|Abr";*/
+		
+		wvGrafico = (WebView) findViewById(R.id.wvGrafico);
+		wvGrafico.loadUrl(strURL);
 	}
 
 	@Override
@@ -23,37 +52,4 @@ public class GraficosActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	/*public boolean onOptionsItemSelected( MenuItem item )
-	{
-		switch (item.getItemId( ))
-		{
-		case R.id.action_zoom_in:
-			mGraphView.zoomIn( ) ;
-			return true ;
-
-		case R.id.action_zoom_out:
-			mGraphView.zoomOut( ) ;
-			return true ;
-
-		case R.id.action_pan_left:
-			mGraphView.panLeft( ) ;
-			return true ;
-
-		case R.id.action_pan_right:
-			mGraphView.panRight( ) ;
-			return true ;
-
-		case R.id.action_pan_up:
-			mGraphView.panUp( ) ;
-			return true ;
-
-		case R.id.action_pan_down:
-			mGraphView.panDown( ) ;
-			return true ;
-		}
-
-		return super.onOptionsItemSelected( item ) ;
-	}*/
-
 }
