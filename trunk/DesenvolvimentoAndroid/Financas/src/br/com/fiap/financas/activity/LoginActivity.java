02eb05d4b011159919a8b0bb0c85170fa88d525c
rodrigotety@gmail.com <rodrigotety@gmail.com@5e7338cb-f527-8b1e-bca7-e1c64bb0f196>
2014-11-03 02:46:07 +0000

    Login com shared preferences

--- trunk/DesenvolvimentoAndroid/Financas/src/br/com/fiap/financas/activity/LoginActivity.java
+++ trunk/DesenvolvimentoAndroid/Financas/src/br/com/fiap/financas/activity/LoginActivity.java
@@ -1,5 +1,10 @@
 package br.com.fiap.financas.activity;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -20,19 +25,62 @@ public class LoginActivity extends Activity {
 	Button btEntrar;
 	EditText edtUsuario;
 	EditText edtSenha;
+	CheckBox checkLembrar;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
 		
 		if (settings.getBoolean("Lembrar", false) != false) {
 			edtUsuario = (EditText) findViewById(R.id.etUsuario);
 			edtSenha = (EditText) findViewById(R.id.etSenhA);
+			checkLembrar = (CheckBox) findViewById(R.id.chkLembrar);
 			edtUsuario.setText(settings.getString("Usuario", ""));
 			edtSenha.setText(settings.getString("Senha", ""));
+			checkLembrar.setChecked(settings.getBoolean("Lembrar", false));
 		}
+		
+		if (settings.getBoolean("Bloqueado", false) == true) {
+			
+			Date dtHoraDesbloqueio = null;
+			Calendar clHoraDesbloqueio = new GregorianCalendar();
+			Calendar clHoraAtual = new GregorianCalendar();
+			
+			String horaBloqueio = settings.getString("HoraBloqueio", "");
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			try {
+				dtHoraDesbloqueio = sdf.parse(horaBloqueio);
+			}
+			catch (Exception e) {
+				Log.e("erro", e.getMessage());
+			}
+			
+			clHoraDesbloqueio.setTime(dtHoraDesbloqueio);
+			
+			//TODO Minute para teste, depois alterar para clHoraDesbloqueio.add(Calendar.HOUR, 1);
+			clHoraDesbloqueio.add(Calendar.MINUTE, 1);
+			
+			// se hora atual posterior hora bloqueio + 1 (desbloqueio)
+			if (clHoraAtual.getTime().after(clHoraDesbloqueio.getTime())) {
+				
+				editor.putBoolean("Bloqueado", false);
+				editor.putInt("Tentativa", 0);
+				editor.commit();
+				
+			}
+			
+			
+			dtHoraDesbloqueio = clHoraDesbloqueio.getTime();
+			String horaDepois = sdf.format(dtHoraDesbloqueio);
+			Log.i("Bloqueio", "Hora do bloqueio: " + horaBloqueio);			
+			Log.i("Bloqueio", "Hora do desbloqueio: " + horaDepois);
+			
+		}
+		
 	}
 
 	@Override
@@ -57,7 +105,7 @@ public class LoginActivity extends Activity {
 
 	public void onClickEntrar(View v) {
 		
-		Log.i("Click", "Clicou botão entrar login");
+		Log.i("Click", "Clicou botao entrar login");
 		
 		Intent i = new Intent(LoginActivity.this, DashboardActivity.class);
 		edtUsuario = (EditText) findViewById(R.id.etUsuario);
@@ -74,22 +122,30 @@ public class LoginActivity extends Activity {
 				Integer tentativa = settings.getInt("Tentativa", 0);
 				tentativa++;
 				editor.putInt("Tentativa", tentativa);
-				alertaLogin(1);
+				if (tentativa >= 3) {
+					editor.putBoolean("Bloqueado", true);	
+					String horaBloqueio = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date());
+					Log.i("HoraBloqueio", horaBloqueio);
+					editor.putString("HoraBloqueio", horaBloqueio);
+					alertaLogin(0);
+				} else		
+					alertaLogin(1);
 			}
 			
 		} else {
 			editor.putString("FirstLogin", "N");
 			editor.putString("Usuario", edtUsuario.getText().toString());
 			editor.putString("Senha", edtSenha.getText().toString());
-			editor.putInt("Tentativas", 0);
+			editor.putInt("Tentativa", 0);
 			editor.putBoolean("Bloqueado", false);
 			
 			startActivity(i);
 		}
 		
-		CheckBox checkLembrar = (CheckBox) findViewById(R.id.chkLembrar);
+		checkLembrar = (CheckBox) findViewById(R.id.chkLembrar);
 		editor.putBoolean("Lembrar", checkLembrar.isChecked());
-		finish();
+		editor.commit();
+
 	}
 	
 	private void alertaLogin(Integer tipo) {
@@ -101,7 +157,7 @@ public class LoginActivity extends Activity {
 		if(tipo == 0){
 			builder.setMessage("Login Bloqueado");
 		} else {
-			builder.setMessage("Erro no usuário ou senha"); 	 
+			builder.setMessage("Erro no usuario ou senha"); 	 
 		}
 		
 		builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
